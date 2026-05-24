d7641d45c11f260d22518766a8ec56693eeb56cc
why459097630 <whywhy9264@gmail.com>
2026-05-24 15:32:59 +0800

    Update layout.tsx

--- src/app/pwa/[storeId]/layout.tsx
+++ src/app/pwa/[storeId]/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { resolveStorePwaPageMetadata } from '@/pwa/storePwaManifest'
 
+export const dynamic = 'force-dynamic'
+
 export async function generateMetadata({
   params
 }: {
